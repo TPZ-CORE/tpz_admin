d9a8e3c5ff5131b22229b7641dc3b0e7daa184d2
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-23 16:40:13 +0300

    Update banned_users.sql

--- tpz_admin/[EXTRA]/banned_users.sql
+++ tpz_admin/[EXTRA]/banned_users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `banned_users` (
   `identifier` varchar(50) NOT NULL,
-  `steamname` varchar(50) DEFAULT NULL,
+  `steamname` varchar(50) NOT NULL,
   `banned` int(1) DEFAULT 0,
   `bannedReason` longtext DEFAULT NULL,
   `warnings` int(11) DEFAULT 0,
   PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
