819a0f33ca39c1d94b1973026922cf12a5ffc58b
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-24 02:56:58 +0300

    Update tp-server_main.lua

--- tpz_admin/server/tp-server_main.lua
+++ tpz_admin/server/tp-server_main.lua
@@ -1,12 +1,5 @@
 
-local TPZ    = exports.tpz_core:getCoreAPI()
-local TPZInv = exports.tpz_inventory:getInventoryAPI()
-
-local PlayersList = {}
-
------------------------------------------------------------
---[[ Local Functions ]]--
------------------------------------------------------------
+local TPZ = exports.tpz_core:getCoreAPI()
 
 -----------------------------------------------------------
 --[[ Functions ]]--
@@ -17,45 +10,20 @@ GetPlayerList = function ()
 end
 
 -----------------------------------------------------------
---[[ Base Events ]]--
------------------------------------------------------------
-
-AddEventHandler('onResourceStop', function(resourceName)
-	if (GetCurrentResourceName() ~= resourceName) then
-        return
-    end
-
-	PlayersList = nil
-
-end)
-
--- When player quits the game, we unload - reset online player data.
-AddEventHandler('playerDropped', function (reason)
-	local _source = source
-	PlayersList[_source] = nil
-end)
-
------------------------------------------------------------
 --[[ Events ]]--
 -----------------------------------------------------------
 
 RegisterServerEvent('tpz_admin:requestUserRoles')
 AddEventHandler('tpz_admin:requestUserRoles', function()
 	local _source = source
-
 	local xPlayer = TPZ.GetPlayer(_source)
 
 	if not xPlayer.loaded() then
 		return
 	end
 
+	local group     = xPlayer.getGroup()
 	local userRoles = xPlayer.getDiscordRoles()
 
-	TriggerClientEvent("tpz_core:updateUserRoles", _source, { userRoles })
-
-	-- When player is loaded, we load the player to the online players list with data.
-	PlayersList[_source]          = {}
-	
-	PlayersList[_source].source   = _source
-	PlayersList[_source].isFrozen = false
+	TriggerClientEvent("tpz_core:updateUserRoles", _source, { userRoles, group })
 end)
