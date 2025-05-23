3f9d1e3913874ea063c2bf68f4fa7b9baf3b866c
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-24 02:43:40 +0300

    Update fxmanifest.lua

--- tpz_admin/fxmanifest.lua
+++ tpz_admin/fxmanifest.lua
@@ -3,7 +3,7 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Nosmakos'
-description 'TPZ-CORE Admin Menu & Actions'
+description 'TPZ-CORE - Admin'
 version '1.0.0'
 
 shared_scripts {
