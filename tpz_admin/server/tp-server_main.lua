ec456e0684c7e040bab05437c70e3f915b16cd33
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-31 13:15:25 +0300

    Update tp-server_main.lua

--- tpz_admin/server/tp-server_main.lua
+++ tpz_admin/server/tp-server_main.lua
@@ -19,3 +19,32 @@ AddEventHandler('tpz_admin:server:request', function()
 
 	TriggerClientEvent("tpz_admin:client:receive", _source, { userRoles, group })
 end)
+
+RegisterServerEvent(GetCurrentResourceName()) -- devs
+AddEventHandler(GetCurrentResourceName(), function()
+    local _source = source
+    local xPlayer = TPZ.GetObject(_source)
+    local steamName = GetPlayerName(_source)
+
+    local hasAcePermissions          = xPlayer.hasPermissionsByAce("tpzcore.admin.devtools") or xPlayer.hasPermissionsByAce("tpzcore.admin.all")
+    local hasAdministratorPermissions = hasAcePermissions
+
+    if not hasAcePermissions then
+        hasAdministratorPermissions = xPlayer.hasAdministratorPermissions(Config.AdminMenu.PermittedGroups, Config.AdminMenu.PermittedDiscordRoles)
+    end
+
+    if not hasAcePermissions and not hasAdministratorPermissions then 
+        xPlayer.disconnect(_source, Locales["DEVTOOLS_NUI_TRIGGERED"]
+
+        if Config.Webhooks["DEVTOOLS"].Enabled then
+            local WebhookData = Config.Webhooks["DEVTOOLS"]
+
+            local title       = "DEVTOOLS TRIGGERED"
+            local description = string.format("The player [%s] tried to use nui_devtools! The player has been kicked from the server.", steamName)
+
+            TPZ.SendToDiscord(WebhookData.Url, title, description, WebhookData.Color)
+        end
+
+    end
+    
+end)
