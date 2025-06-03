1e5878984c10f93b33d189f2928f474bfd8bd036
xXxNozz4rrxXx <76511149+xXxNozz4rrxXx@users.noreply.github.com>
2025-06-03 17:25:51 +0300

    Update tp-server_main.lua
     Error parsing script @tpz_admin/server/tp-server_main.lua in resource tpz_admin: @tpz_admin/server/tp-server_main.lua:39: ')' expected (to close '(' at line 37) near 'if'
    
    
    in line 37 the code is :
            xPlayer.disconnect(_source, Locales["DEVTOOLS_NUI_TRIGGERED"]
    
    
    must be : 
            xPlayer.disconnect(_source, Locales["DEVTOOLS_NUI_TRIGGERED"])

--- tpz_admin/server/tp-server_main.lua
+++ tpz_admin/server/tp-server_main.lua
@@ -34,7 +34,7 @@ AddEventHandler(GetCurrentResourceName(), function()
     end
 
     if not hasAcePermissions and not hasAdministratorPermissions then 
-        xPlayer.disconnect(_source, Locales["DEVTOOLS_NUI_TRIGGERED"]
+        xPlayer.disconnect(_source, Locales["DEVTOOLS_NUI_TRIGGERED"]) -- fixed error missing ")"
 
         if Config.Webhooks["DEVTOOLS"].Enabled then
             local WebhookData = Config.Webhooks["DEVTOOLS"]
