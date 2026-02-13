b987a15b2ead64a8a870b9a43231a0e2c1554c11
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-02-13 14:33:52 +0200

    Update fxmanifest.lua

--- tpz_admin/fxmanifest.lua
+++ tpz_admin/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE - Admin'
-version '1.0.2'
+version '2.0.0'
 
 ui_page 'html/index.html'
 
@@ -32,4 +32,5 @@ dependencies {
 }
 
 lua54 'yes'
+
 
